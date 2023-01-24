cc7a5657accf87745d7981f5321f00833cbe43ab
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2023-01-24 18:13:14 +0100

    end of day

--- app.py
+++ app.py
@@ -16,11 +16,22 @@ user_input = input()
 # compare user input to the column 'pinyin'
 if user_input == card_data['pinyin']:
     print('Correct!')
+    # Calculate next review date (10 minutes)
+    next_review_date = time.time() + 10*60
     # update card_status table
-    db.execute_query(f"UPDATE card_status SET ladder_status_id = ladder_status_id + 1, next_review_date = CURRENT_TIMESTAMP + INTERVAL '6' DAY WHERE card_id = {card_data['card_id']}")
+    # Connect to the database
+    conn, c = db.connect_database()
+    db.execute_query(f"UPDATE card_status SET ladder_status_id = ladder_status_id + 1, next_review_date = '{next_review_date}' WHERE card_id = {card_data['card_id']}")
+    db.commit_and_close_connection(conn)
 else:
     print('Incorrect! The correct answer is: ' + card_data['pinyin'])
+    # Calculate next review date (10 seconds)
+    next_review_date = time.time() + 10
     # update card_status table
-    db.execute_query(f"UPDATE card_status SET ladder_status_id = ladder_status_id + 1, next_review_date = CURRENT_TIMESTAMP + INTERVAL '6' DAY WHERE card_id = {card_data['card_id']}")
+    # Connect to the database
+    conn, c = db.connect_database()
+    db.execute_query(f"UPDATE card_status SET ladder_status_id = 1, next_review_date = '{next_review_date}' WHERE card_id = {card_data['card_id']}")
+    db.commit_and_close_connection(conn)
 
 # Give user feedback
+print('You have successfully updated the card status!')
--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import time
 
 def connect_database():
   # Create a connection to the database
@@ -14,22 +15,26 @@ def execute_query(query):
     rows = c.fetchone()
 
     # Create a dictionary from the data
-    card_data = {
-        'card_id': rows[0],
-        'chinese_character': rows[1],
-        'translation_english': rows[2],
-        'pinyin': rows[3],
-        'hsk_level': rows[4]
-    }
-
-    # Print the dictionary
-    # print(card_data)
-    # Commit changes
-    conn.commit()
-    # Close connection
-    conn.close()
-    # Return the dictionary
-    return card_data
+    if rows is not None:
+        card_data = {
+            'card_id': rows[0],
+            'chinese_character': rows[1],
+            'translation_english': rows[2],
+            'pinyin': rows[3],
+            'hsk_level': rows[4]
+        }
+        # Commit changes
+        conn.commit()
+        # Close connection
+        conn.close()
+        # Return the dictionary
+        return card_data
+    else:
+        conn.commit()
+        # Close connection
+        conn.close()
+        # Return None if no results
+        return None
 
 def commit_and_close_connection(conn):
     # Commit changes
@@ -40,9 +45,5 @@ def commit_and_close_connection(conn):
 def add_card_to_status_table(card_id):
     conn, c = connect_database()
     current_date = time.strftime("%Y-%m-%d")
-    c.execute(f"INSERT INTO card_status (card_id, date_last_reviewed, ladder_status_id, next_review_date) VALUES ({card_id}, '{current_date}', 1, CURRENT_TIMESTAMP + INTERVAL 6 DAY);")
+    c.execute(f"INSERT INTO card_status (card_id, date_last_reviewed, ladder_status_id, next_review_date) VALUES ({card_id}, '{current_date}', 1, CURRENT_TIMESTAMP + INTERVAL 10 MINUTE);")
     commit_and_close_connection(conn)
-
-conn, c = connect_database()
-
-commit_and_close_connection(conn)
